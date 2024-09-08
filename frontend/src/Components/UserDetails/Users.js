e2dc8fe18b62ba387e4c6874fbdd162753258adf
TharuWitharana <tharushiwitharana@gmail.com>
2024-09-08 21:29:25 +0530

    Implimented search fuctionality and send whatsapp messages

--- frontend/src/Components/UserDetails/Users.js
+++ frontend/src/Components/UserDetails/Users.js
@@ -28,6 +28,27 @@ function Users(){
       onAfterPrint:()=>alert("User report succesfully downloaded")
     });
 
+    //search func imlimentation
+    const [searchQuery, setSearchQuery] = useState("");
+    const [noResults, setNoResults] = useState(false);
+
+    const handleSearch=()=>{
+      fetchHandler().then((data) => { 
+        const filteredUsers=data.users.filter((user)=>Object.values(user).some((field)=>field.toString().toLowerCase().includes(searchQuery.toLowerCase())));
+        setUsers(filteredUsers);
+        setNoResults(filteredUsers.length===0);
+    });
+  }
+
+
+  const handleSendReport=()=>{
+    const phoneNumber="+94702665670";
+    const message="User report downloaded successfully";
+    const WhatsAppURL=`https://web.whatsapp.com/send?pnone=${phoneNumber}&text=${encodeURIComponent(message)}`;
+    
+    window.open(WhatsAppURL,"_blank");
+  }
+
   return (
     <div>
       <Nav />
@@ -35,6 +56,13 @@ function Users(){
         <h1 >
           All Users
         </h1>
+        <input type="text" placeholder="Search Users" onChange={(e) => setSearchQuery(e.target.value)}></input>
+        <button onClick={handleSearch}>Search</button>
+
+       
+        {noResults? (<div><p>No Results Found</p></div>):(
+
+
         <div ref={componentRef}>
           {users && users.map((user, i) => (
             <div key={i}>
@@ -42,8 +70,11 @@ function Users(){
             </div>
           ))}
         </div>
+        )}
       </div>
       <button onClick={handlePrint}>Download Report</button>
+      <br></br>
+      <button onClick={handleSendReport}>Send WhatsApp Message</button>
     </div>
   )
 }
